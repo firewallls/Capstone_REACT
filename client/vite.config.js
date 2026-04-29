6faed130ee8a6f572df10d8f1be41510bf3a922a
moksh <muktimoksh23@gmail.com>
2026-04-30 01:21:28 +0530

    setup shadcn

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
+import { fileURLToPath } from "url"
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss()
-  ],
-  base: '/Capstone_REACT/'
+    plugins: [react(), tailwindcss()],
+    base: '/Capstone_REACT/',
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
